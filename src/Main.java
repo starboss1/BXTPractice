b0335381791b2d40cfb307553968a55e76ee1d74
Sergei <sergei.konoshenko2000@gmail.com>
2019-03-01 23:25:45 +0200

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 
 public class Main {
+	
+	private static void doSomething() {
+		int x = 555;
+		String st = "string";
+		
+		System.out.println(x);
+	}
 
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!!!!!!!");
+		doSomething();
 	}
 }
