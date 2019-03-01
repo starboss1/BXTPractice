8399f6eeb1155970df9a0347da635ba1f8e7de7c
Sergei <sergei.konoshenko2000@gmail.com>
2019-03-01 23:46:24 +0200

    Pls, work

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,16 @@
 public class Main {
 	
 	private static void doSomething() {
-		int x = 555;
-		String st = "string";
-		
-		System.out.println(x);
+
 	}
 
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!!!!!!!");
 		doSomething();
+		int x = 555;
+		String st = "string";
+		
+		System.out.println(x);
 	}
 }
